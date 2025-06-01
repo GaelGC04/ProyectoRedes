5f58dfa674f18a853b5a8ba70d15e5465e1ab5a7
Jorge Garcia <garciajorgecf@gmail.com>
2025-05-31 20:48:21 -0600

    Añadiendo clase principal de chat con los objetivos del proyecto

---
+++ src/chat/Chat.java
@@ -0,0 +1,12 @@
+package chat;
+
+public class Chat {
+    public static void main(String [] args) {
+        // Un chat simple, que se ejecute en una ventana, que sea similara la interfaz de whatsapp
+        // barra para texto, un boton para enviar el texto y un enlace para añadir archivos
+        // una vez que se añada el archivo que se envie automaticamente
+        // si es archivo que sea tcp
+        // si es mensaje que sea udp
+        // en caso de hacer udp que se muestre el proceso de envio y tiempos y bps y etc
+    }
+}
