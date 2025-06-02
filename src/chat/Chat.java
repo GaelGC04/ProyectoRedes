845f20ffef08af02f80a4f87a854b5ca034a9167
Jorge Garcia <garciajorgecf@gmail.com>
2025-06-01 20:59:52 -0600

    Añadiendo validación de nombre de usuario

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -10,6 +10,10 @@ public class Chat {
             String nombre = "";
             do {
                 nombre = JOptionPane.showInputDialog(null, "Ingresa tu nombre:", "Nombre de usuario", JOptionPane.QUESTION_MESSAGE);
+                if (nombre.contains(",") || nombre.contains(";")) {
+                    JOptionPane.showMessageDialog(null, "Favor de no ingresar ',' ni ';'", "Valores inválidos", 0);
+                    nombre = "";
+                }
             } while (nombre == null || nombre.trim().isEmpty());
 
             VentanaContactos.cargarContactos(nombre);
